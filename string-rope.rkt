efd342211fc5a76bbac18ca40e7bebddaed16859
Eric Griffis <dedbox@gmail.com>
2026-08-31 19:34:17 -0700

    add chunk-overlap=? override to string rope type

--- string-rope.rkt
+++ string-rope.rkt
@@ -7,14 +7,17 @@
 (provide (all-defined-out))
 
 (define-rope-type string
-  #:chunk?       string?
-  #:chunk-limit  512
-  #:chunk-empty  ""
-  #:chunk-length string-length
-  #:chunk-ref    string-ref
-  #:chunk-slice  (λ (c i k) (substring c i (+ i k)))
-  #:chunk-append (λ (cs) (apply string-append cs))
-  #:elem-width   1
-  #:elem-hash    char->integer)
+  #:chunk?          string?
+  #:chunk-limit     512
+  #:chunk-empty     ""
+  #:chunk-length    string-length
+  #:chunk-ref       string-ref
+  #:chunk-slice     (λ (c i k) (substring c i (+ i k)))
+  #:chunk-append    (λ (cs) (apply string-append cs))
+  #:chunk-overlap=? (λ (ac bc ap bp k)
+                      (for/and ([i (in-range k)])
+                        (char=? (string-ref ac (+ ap i)) (string-ref bc (+ bp i)))))
+  #:elem-width      1
+  #:elem-hash       char->integer)
 
 (require rope2/generic-ops)
